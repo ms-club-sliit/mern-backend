686fb26bfb207ac893da552ab804799a60f100d5
rusiruavb <rusiruavb98@gmail.com>
2022-02-25 20:59:06 +0530

    Add update and remove functions

--- controllers/Employee.controller.js
+++ controllers/Employee.controller.js
@@ -28,7 +28,46 @@ const getEmployees = async (req, res) => {
   }
 };
 
+const updateEmployee = async (req, res) => {
+  const empId = req.params.id;
+
+  try {
+    const employee = await Employee.findById(empId);
+
+    if (!employee) {
+      return res.status(404).json("There is no employee");
+    }
+
+    const { name, position, salary } = req.body;
+
+    const emp = await Employee.findByIdAndUpdate(empId, { name, position, salary });
+
+    res.status(200).json(emp);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+const removeEmployee = async (req, res) => {
+  const empId = req.params.id;
+
+  try {
+    const employee = await Employee.findById(empId);
+
+    if (!employee) {
+      return res.status(404).json("There is no employee to remove");
+    }
+
+    const removedEmployee = await Employee.findByIdAndDelete(empId);
+    res.status(200).json(removedEmployee);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
 module.exports = {
   addEmployee,
   getEmployees,
+  updateEmployee,
+  removeEmployee,
 };
--- routes/Employee.route.js
+++ routes/Employee.route.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  addEmployee,
-  getEmployees,
-} = require("../controllers/Employee.controller");
+const { addEmployee, getEmployees, updateEmployee, removeEmployee } = require("../controllers/Employee.controller");
 
 //@route GET api/employee/all
 //@desc Get all employees
@@ -12,5 +9,13 @@ router.get("/all", getEmployees);
 //@route POST api/employee
 //@desc Add an employee
 router.post("/", addEmployee);
+
+//@route PUT api/employee/:id
+//@desc Update Employee
+router.put("/:id", updateEmployee);
+
+//@route DELETE api/employee/:id
+//@desc Update Employee
+router.delete("/:id", removeEmployee);
 
 module.exports = router;
